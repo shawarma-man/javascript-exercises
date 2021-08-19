06b063e487eb6a5433e72ae808161651db773009
shawarma-man <87611022+shawarma-man@users.noreply.github.com>
2021-08-19 21:21:28 +0300

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  let i=0;
+  let cpy_str = "";
+  while(i<num){
+    cpy_str=cpy_str+str;
+  }
+  return cpy_str;
 };
 
 module.exports = repeatString;
